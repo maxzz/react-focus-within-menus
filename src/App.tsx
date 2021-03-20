5ecd4b14a70409c72f67e0e2b0982365cc874e58
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-19 19:46:44 -0700

    Feature: add second demo; fixed React complains about SVG

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import FocusWithinMenuDemo from "./components/FocusWithinMenuDemo";
+import TrackingMenuDemo from "./components/TrackingMenuDemo";
 
 function App() {
     return <div className="App h-screen bg-gray-200">
         <FocusWithinMenuDemo></FocusWithinMenuDemo>
+        <TrackingMenuDemo></TrackingMenuDemo>
     </div>;
 }
 
--- src/components/TrackingMenuDemo.tsx
+++ src/components/TrackingMenuDemo.tsx
@@ -1,69 +1,89 @@
 import React from 'react';
 import './FocusWithinMenuDemo.scss';
 
-interface FocusWithinMenuProps {
+interface TrackingMenuProps {
 
 }
 
-const FocusWithinMenu: React.FC<FocusWithinMenuProps> = () => {
+const TrackingMenu: React.FC<TrackingMenuProps> = () => {
     return (
-        <nav role="navigation">
-            <ul>
-                <li><a href="#" aria-haspopup="true">Top menu Mars</a>
-                    <ul className="dropdown" aria-label="submenu">
-                        <li><a className="text-sm" href="#">Item-1</a></li>
-                        <li><a className="text-sm" href="#">Item-2</a></li>
-                        <li><a className="text-sm" href="#">Item-3</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Neptun</a></li>
-                <li><a href="#">Earth</a>
-                    <ul className="dropdown" aria-label="submenu">
-                        <li>
-                            <a href="#">Submenu-3-Item-1</a>
-                            <div className="px-3 py-2 border rounded-md">
-                                <h2 className="font-bold">Sub title</h2>
-                                <p className="text-sm">Lorem ipsum dolor sit amet.</p>
-                            </div>
-                        </li>
-                        <li><a href="#">Submenu-3-Item-2</a></li>
-                        <li><a href="#">Submenu-3-Item-3</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Black</a>
-                    <ul className="dropdown" aria-label="submenu">
-                        <li><a href="#">Item-1</a></li>
-                        <li><a href="#">Item-2</a></li>
-                    </ul>
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" hidden>
+                <symbol id="arrow" viewBox="0 0 16 16" >
+                    <polyline points="4 6, 8 10, 12 6" stroke="#000" strokeWidth="2" fill="transparent" strokeLinecap="round" />
+                </symbol>
+            </svg>
 
-                </li>
-            </ul>
-        </nav>
+            {/* <!-- In the real world, all hrefs would have go to real, unique URLs, not a "#" --> */}
+            <nav id="site-navigation" className="site-navigation" aria-label="Clickable Menu Demonstration">
+                <ul className="main-menu clicky-menu no-js">
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Services
+				            <svg aria-hidden="true" width="16" height="16">
+                                <use xlinkHref="#arrow" />
+                            </svg>
+                        </a>
+                        <ul>
+                            <li><a href="#">Design</a></li>
+                            <li><a href="#">Development</a></li>
+                            <li><a href="#">Accessibility</a></li>
+                            <li><a href="#">Content Strategy</a></li>
+                            <li><a href="#">Training</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Portfolio
+				            <svg aria-hidden="true" width="16" height="16">
+                                <use xlinkHref="#arrow" />
+                            </svg>
+                        </a>
+                        <ul>
+                            <li><a href="#">Nonprofits</a></li>
+                            <li><a href="#">Higher Education</a></li>
+                            <li><a href="#">Associations</a></li>
+                            <li><a href="#">Consultants</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">
+                            About
+				            <svg aria-hidden="true" width="16" height="16">
+                                <use xlinkHref="#arrow" />
+                            </svg>
+                        </a>
+                        <ul>
+                            <li><a href="#">Mission</a></li>
+                            <li><a href="#">History</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     );
 };
 
-interface FocusWithinMenuDemoProps {
+interface TrackingMenuDemoProps {
 
 }
 
-const FocusWithinMenuDemo: React.FC<FocusWithinMenuDemoProps> = () => {
+const TrackingMenuDemo: React.FC<TrackingMenuDemoProps> = () => {
     const [visible, setVisible] = React.useState(false);
     return (
         <div className="pt-4">
             <h1>
                 <button className="ml-4 px-4 py-3 font-bold text-gray-100 bg-gray-500 hover:bg-gray-600 focus:outline-none border border-gray-100 rounded-md"
                     onClick={() => setVisible(!visible)}
-                >:focus-within demo</button>
+                >javascript menu tracking demo</button>
             </h1>
 
             {visible && <div className="mt-4 flex justify-between items-center bg-gradient-to-r from-green-400 to-blue-500">
                 <span className="pl-4">
-
-                    {/* <svg className="w-12 h-12 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path fill="currentColor" d="M12 14l9-5-9-5-9 5 9 5z" />
-                        <path fill="bg-red-100" d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
-                    </svg> */}
 
                     <svg className="w-12 h-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 223.84 243.51">
                         <defs>
@@ -106,11 +126,11 @@ const FocusWithinMenuDemo: React.FC<FocusWithinMenuDemoProps> = () => {
                 </span>
 
                 <div id="fwin-menu-demo" className="clearfix pr-2">
-                    <FocusWithinMenu></FocusWithinMenu>
+                    <TrackingMenu></TrackingMenu>
                 </div>
             </div>}
         </div>
     );
 };
 
-export default FocusWithinMenuDemo;
+export default TrackingMenuDemo;
