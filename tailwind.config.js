5d9d4448d0c71bf159a252ebcf7d9846b967fb5c
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-19 17:41:32 -0700

    Feature: add sass, tailwind, CSS base demo

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: [
+        '*.html',
+        './src/**/*.{js,jsx,ts,tsx,vue}',
+      ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
