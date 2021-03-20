d59072a64d90277342a80f16e0ea6bc1c52a8436
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-19 17:29:34 -0700

    Fix: basic clean up of the default app

---
+++ src/components/FocusWithinMenu.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface FocusWithinMenuProps {
+    
+}
+
+const FocusWithinMenu: React.FC<FocusWithinMenuProps> = () => {
+    return (
+        <div>
+            aa
+        </div>
+    );
+};
+
+export default FocusWithinMenu;
