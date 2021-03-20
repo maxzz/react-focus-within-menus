2a3e4d12eda07acf566457c01d940a4b8280fa43
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-20 02:27:58 -0700

    Fix: add update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig, UserConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 
 export default ({ command }: { command: string }) => {
-    let config = defineConfig({
+    return defineConfig({
         base: command === 'build' ? '/react-focus-within-menus/' : '/',
         plugins: [reactRefresh()],
-    }) as UserConfig;
-    return config;
+    });
 }
