ca2018fedebc0ce3a9c809d0659baa99a7564ac6
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-31 20:44:39 -0700

    Feature: complete ClickOutside

--- src/components/ClickOutside.tsx
+++ src/components/ClickOutside.tsx
@@ -35,41 +35,41 @@ function ClickOutside() {
                 className={
                     `my-4 px-3 py-2 border rounded border-green-300 bg-gray-500 uppercase tracking-wide
                     bg-gradient-to-r ${isOpen ? 'from-blue-600 to-blue-400' : 'from-blue-400 to-blue-600'}
+                    flex
                     focus:outline-none
                     `
                 }
                 type='button'
                 onClick={event => {
                     event.stopPropagation();
-                    console.log('Click', isOpen, Date.now());
-
                     setIsOpen(isOpen => !isOpen);
                 }}
                 aria-expanded={isOpen}
                 aria-controls='nav-content'
             >
                 Extra actions
-                <div className="">
+                <div className="w-6 h-6 ml-4 text-blue-800">
                     {!isOpen ?
-                    (<svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
-                    </svg>) :
-                    (<svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 5l7 7-7 7M5 5l7 7-7 7" />
-                    </svg>)
+                        (<svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
+                        </svg>)
+                    :
+                        (<svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 5l7 7-7 7M5 5l7 7-7 7" />
+                        </svg>)
                     }
                 </div>
             </button>
             {/* nav-content */}
             <div className={`${!isOpen ? 'hidden' : ''}`} aria-hidden={!isOpen} aria-labelledby='nav-toggle'>
                 <ul className="flex space-x-4">
-                    <li className="px-4 py-2 text-gray-700 hover:bg-yellow-300 uppercase">
+                    <li className="px-5 py-3 text-gray-700 hover:bg-yellow-300 uppercase text-gray-200">
                         <a href='#'>A</a>
                     </li>
-                    <li className="px-4 py-2 text-gray-700 hover:bg-yellow-400 uppercase">
+                    <li className="px-5 py-3 text-gray-700 hover:bg-yellow-400 uppercase text-gray-200">
                         <a href='#'>B</a>
                     </li>
-                    <li className="px-4 py-2 text-gray-700 hover:bg-yellow-400 uppercase">
+                    <li className="px-5 py-3 text-gray-700 hover:bg-yellow-400 uppercase text-gray-200">
                         <a href='#'>C</a>
                     </li>
                 </ul>
