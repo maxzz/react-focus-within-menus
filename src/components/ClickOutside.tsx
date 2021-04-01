eaa0027f4725e36731b425baffc4784c07fc92e6
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-31 19:58:53 -0700

    Fix: code clean up

--- src/components/ClickOutside.tsx
+++ src/components/ClickOutside.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const useAutoClose = ({ setIsOpen, menu }: { setIsOpen: React.Dispatch<React.SetStateAction<boolean>>, menu: React.RefObject<Element> }) => {
+const useAutoClose = ({ setIsOpen, menu }: { setIsOpen: React.Dispatch<React.SetStateAction<boolean>>, menu: React.RefObject<Element>; }) => {
     const handleClosure = React.useCallback((event: MouseEvent | FocusEvent) => {
-        console.log('Contains', menu.current?.contains(event.target as Node));
-        
         !menu.current?.contains(event.target as Node) && setIsOpen(false);
     }, [setIsOpen, menu]);
 
@@ -24,47 +22,44 @@ function ClickOutside() {
 
     useAutoClose({ setIsOpen, menu });
 
-    console.log('---------------', isOpen, Date.now());
-
     return (
-            <div className="c" ref={menu} >
-                {/* nav-toggle */}
-                <button
-                    className="my-4 px-2 py-1 border rounded border-gray-300 bg-gray-500"
-                    type='button'
-                    aria-expanded={isOpen}
-                    aria-controls='nav-content'
-                    onClick={event => {
-                        event.stopPropagation();
-                        console.log('Click', isOpen, Date.now());
-                
-                        setIsOpen(isOpen => !isOpen);
-                        // setIsOpen(!isOpen);
-                    }}
-                >
-                    Navigation
-                </button>
-                        <nav role='navigation'
-                className="
-                    my-4 mx-auto max-w-xl px-4 text-gray-300 flex items-center justify-between
-                    bg-gradient-to-r from-green-400 to-blue-500"
-                        >
-                {/* nav-content */}
-                <div className={`${!isOpen ? 'hidden' : ''}`} aria-hidden={!isOpen} aria-labelledby='nav-toggle'>
-                    <ul className="flex space-x-4">
-                        <li className="px-4 py-2 text-gray-700 hover:bg-yellow-400">
-                            <a href='#'>Link 1</a>
-                        </li>
-                        <li className="px-4 py-2 text-gray-700 hover:bg-yellow-400">
-                            <a href='#'>Link 2</a>
-                        </li>
-                        <li className="px-4 py-2 text-gray-700 hover:bg-yellow-400">
-                            <a href='#'>Link 3</a>
-                        </li>
-                    </ul>
-                </div>
-                        </nav>
+        <nav
+            ref={menu}
+            className="
+                my-4 mx-auto max-w-xl px-4
+                text-gray-300 flex items-center justify-between bg-gradient-to-r from-green-400 to-blue-500"
+            role='navigation'
+        >
+            {/* nav-toggle */}
+            <button
+                className="my-4 px-2 py-1 border rounded border-gray-300 bg-gray-500 uppercase tracking-wide bg-gradient-to-r from-blue-600 to-blue-400"
+                type='button'
+                onClick={event => {
+                    event.stopPropagation();
+                    console.log('Click', isOpen, Date.now());
+
+                    setIsOpen(isOpen => !isOpen);
+                }}
+                aria-expanded={isOpen}
+                aria-controls='nav-content'
+            >
+                Extra controls
+            </button>
+            {/* nav-content */}
+            <div className={`${!isOpen ? 'hidden' : ''}`} aria-hidden={!isOpen} aria-labelledby='nav-toggle'>
+                <ul className="flex space-x-4">
+                    <li className="px-4 py-2 text-gray-700 hover:bg-yellow-300 uppercase">
+                        <a href='#'>Link 1</a>
+                    </li>
+                    <li className="px-4 py-2 text-gray-700 hover:bg-yellow-400 uppercase">
+                        <a href='#'>Link 2</a>
+                    </li>
+                    <li className="px-4 py-2 text-gray-700 hover:bg-yellow-400 uppercase">
+                        <a href='#'>Link 3</a>
+                    </li>
+                </ul>
             </div>
+        </nav>
     );
 }
 
